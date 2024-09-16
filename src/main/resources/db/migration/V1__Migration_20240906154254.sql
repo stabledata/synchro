02126390391f5ab043270602649006c235990651
cif <email@benipsen.com>
2024-09-16 21:56:30 +0100

    chore: super pared down initial migration

--- src/main/resources/db/migration/V1__Migration_20240906154254.sql
+++ src/main/resources/db/migration/V1__Migration_20240906154254.sql
@@ -4,45 +4,43 @@ CREATE SCHEMA IF NOT EXISTS stable;
 
 /* Default stores */
 CREATE TABLE stable.logs (
-    id UUID PRIMARY KEY,
-    collection_slug text,
-    field_id UUID,
-    document_id UUID,
-    event_type TEXT NOT NULL,
-    actor_id TEXT NOT NULL,
-    created_at BIGINT NOT NULL,
-    confirmed_at BIGINT,
-    -- todo: more thoughts on log storage, diffs undo etc.
-    content JSON
+    id uuid PRIMARY KEY,
+    actor_id text NOT NULL,
+    path text,
+    collection_id uuid,
+    document_id uuid,
+    event_type text NOT NULL,
+    created_at bigint NOT NULL,
+    confirmed_at bigint
 );
 
-CREATE INDEX index_logs_collection_slug ON stable.logs (collection_slug);
+CREATE INDEX index_logs_collection_id ON stable.logs (collection_id);
 CREATE INDEX index_logs_document_id ON stable.logs (document_id);
 
 CREATE TABLE stable.collections (
     id uuid PRIMARY KEY,
-    name text,
-    slug text NOT NULL,
-    icon text,
+    path text NOT NULL,
     type text,
+    label text,
+    icon text,
     description text
 );
 
 CREATE TABLE stable.fields (
    id uuid PRIMARY KEY,
-   collection_slug text,
+   collection_id uuid NOT NULL,
+   path text NOT NULL,
    kind text NOT NULL,
-   name text NOT NULL,
-   default_value text,
+
    label text,
-   realtime boolean,
    placeholder text,
+   default_value text,
+
+   realtime boolean,
    help json,
+
    disabled boolean,
-   hidden boolean,
-   width text,
-   preview_type text,
-   choices json
+   hidden boolean
 );
 
-CREATE INDEX index_fields_collection_slug ON stable.fields(collection_slug text_ops);
+CREATE INDEX index_fields_collection_slug ON stable.fields(collection_id);
