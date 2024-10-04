270d86953333aca686c718bc33c90e5a22c6be30
cif <email@benipsen.com>
2024-10-04 15:41:56 -0400

    chore: try to map repo id specifically

--- infra/cici.tf
+++ infra/cici.tf
@@ -18,7 +18,7 @@ resource "google_iam_workload_identity_pool_provider" "github_actions_provider" 
   attribute_condition = "assertion.repository_owner == 'stabledata'"
   attribute_mapping = {
     "google.subject"       = "assertion.sub"
-    "attribute.repository" = "assertion.repository"
+    "attribute.repository_id" = "assertion.repository_id"
 #    "attribute.aud" = "assertion.aud"
   }
 
@@ -36,13 +36,13 @@ resource "google_service_account" "github_cicd_service_account" {
 resource "google_service_account_iam_member" "allow_github_to_impersonate" {
   service_account_id = google_service_account.github_cicd_service_account.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_actions_pool.workload_identity_pool_id}/attribute.repository/stabledata/*"
+  member             = "principalSet://iam.googleapis.com/projects/${var.project}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_actions_pool.workload_identity_pool_id}/attribute.repository_id/R_kgDOMnXo9g"
 }
 
 resource "google_service_account_iam_member" "allow_github_to_create_sa_tokens" {
   service_account_id = google_service_account.github_cicd_service_account.name
   role               = "roles/iam.serviceAccountTokenCreator"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_actions_pool.workload_identity_pool_id}/attribute.repository/stabledata/*"
+  member             = "principalSet://iam.googleapis.com/projects/${var.project}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_actions_pool.workload_identity_pool_id}/attribute.repository_id/R_kgDOMnXo9g"
 }
 
 resource "google_project_iam_member" "allow_push_to_artifact_registry" {
